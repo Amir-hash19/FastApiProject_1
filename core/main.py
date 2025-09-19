551b0f132756feaef43216a67e059d7bd208c05c
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2025-09-19 12:31:50 +0330

    add data :arrow_up:

--- core/main.py
+++ core/main.py
@@ -3,5 +3,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+data = {
+    1:{
+        "description":"payed for rent",
+        "amount":100
+    },
+
+    2:{
+        "description":"payed for car ",
+        "amount":1000
+    }
+    
+}
+
 
 

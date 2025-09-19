4411c88b35809f2f52d9811d47ba229ab2282ce7
amirhoseinheydari <amirhosein.hydri1381@email.com>
2025-09-19 12:50:57 +0330

    add retrieve api for payments :package:

--- core/main.py
+++ core/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Path
 from fastapi.responses import JSONResponse
 import random
 
@@ -21,4 +21,17 @@ data = {
 
 @app.get("/payments")
 def get_payments():
+
+    """ This API return all of the payments in form of the json response """
+
     return JSONResponse(content=data, status_code=status.HTTP_200_OK)
+
+
+
+@app.get("/payment/{payment_id}")
+def retrieve_payment_detail(payment_id: int = Path(title="payment id"
+                            ,description="The id of the Payment in data")):
+    if payment_id in data:
+        return data[payment_id]
+    else:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Payment not found")

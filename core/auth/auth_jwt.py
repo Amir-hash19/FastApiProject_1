8ee123bfc6fb055e9c420cda6820bfd53eb3d640
amirhoseinheydari <amirhosein.hydri1381@email.com>
2025-09-30 13:13:38 +0330

    add JWT_Auth and cofig file

---
+++ core/auth/auth_jwt.py
@@ -0,0 +1,106 @@
+from datetime import datetime,timedelta, timezone
+from jwt import InvalidSignatureError, DecodeError
+from fastapi import Depends
+from fastapi import HTTPException, status
+import jwt
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
+from ..database import User, Payment
+from ..config import settings
+from ..database import get_db
+
+
+security = HTTPBearer()
+
+
+def generate_access_token(user_id: int, expires_in: int = 60*5) -> str:
+    now = datetime.now(timezone.utc)
+    payload = {
+        "user_id": user_id,
+        "iat": int(now.timestamp()),
+        "exp": int((now + timedelta(seconds=expires_in)).timestamp()),
+        "type": "access"
+    }
+    return jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm="HS256")
+
+
+def generate_refresh_token(user_id: int, expires_in: int = 3600*24) -> str:
+    now = datetime.now(timezone.utc)
+    payload = {
+        "user_id": user_id,
+        "iat": int(now.timestamp()),
+        "exp": int((now + timedelta(seconds=expires_in)).timestamp()),
+        "type": "refresh"
+    }
+    return jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm="HS256")
+
+
+
+
+def get_authenticated_user(
+        credentials: HTTPAuthorizationCredentials = Depends(security),
+        db: Session = Depends(get_db)
+    ):
+    token = credentials.credentials
+    try:
+        decoded = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+        if decoded.get("type") != "access":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token type")
+
+        if datetime.now(timezone.utc) > datetime.fromtimestamp(decoded["exp"], tz=timezone.utc):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+
+        user_id = decoded.get("user_id")
+        if not user_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Missing user_id in token")
+
+        user_obj = db.query(User).filter_by(id=user_id).one()
+        return user_obj
+
+    except (InvalidSignatureError, DecodeError):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed")
+
+    
+
+
+def decode_refresh_token(token):
+    try:
+        decoded = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+        user_id = decoded.get("user_id",None)
+        if not user_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Authentication failed, user_id not in the payload")
+        
+        if decoded.get("type") != "refresh":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Authentication failed,token type not valid")
+        
+        if datetime.now(timezone.utc) > datetime.fromtimestamp(decoded["exp"], tz=timezone.utc):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Authentication failed, token expired")
+        
+        return user_id
+        
+
+    except InvalidSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Authentication failed, invalid signature")
+    except DecodeError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Authenication failed, decode failed")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Authentication failed, {e}")     
+    
+
+
+
+
+
+# def get_authenticated_admin(user: UserModel = Depends(get_authenticated_user)):
+#     if user.user_type == UserType.ADMIN:
+#         return user
+
+#     raise HTTPException(
+#         status_code=status.HTTP_403_FORBIDDEN,
+#         detail="Forbidden access to endpoint",
+#     )
+
+
+
--- core/database.py
+++ core/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, String, Integer, DateTime ,Numeric, ForeignKey
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from passlib.context import CryptContext
 from datetime import datetime
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
@@ -14,8 +15,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-
-
+pwd_context = CryptContext(schemes=["argon2"],deprecated="auto")
 
 class User(Base):
     __tablename__ = "users"
@@ -23,11 +23,25 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     full_name = Column(String, index=True)
+    password = Column(String)
     created_at = Column(DateTime, default=datetime.now)
     national_id = Column(Integer,nullable=False)
     updated_date = Column(DateTime(),default=datetime.now,onupdate=datetime.now)
 
     payments = relationship("Payment", back_populates="user", cascade="all, delete-orphan")
+
+
+    def hash_password(self, plain_password: str) -> str:
+        return pwd_context.hash(plain_password)
+    
+
+    def verify_password(self, plain_password: str)-> bool:
+        return pwd_context.verify(plain_password, self.password)
+    
+
+    def set_password(self, plain_text: str) -> None:
+        self.password = self.hash_password(plain_text)
+        
 
 
     def __repr__(self):
@@ -48,3 +62,12 @@ class Payment(Base):
 
     def __repr__(self):
         return f"<Payment(id={self.id}, user_id={self.user_id}, amount={self.amount})>"
+    
+
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()    

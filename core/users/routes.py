f137397b0be5985728a34923ad13bb87d1cfb9da
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2025-10-01 21:17:33 +0330

    layout of the project changed

--- core/users/routes.py
+++ core/users/routes.py
@@ -1,4 +1,4 @@
-from core.auth.auth_jwt import generate_access_token, generate_refresh_token, decode_refresh_token
+from core.auth.auth_jwt import generate_access_token, generate_refresh_token, decode_refresh_token, get_authenticated_user
 from fastapi import FastAPI, status, HTTPException, Path, Request,Response,Depends, APIRouter
 from fastapi.responses import JSONResponse, Response
 from core.users.models import pwd_context
@@ -9,45 +9,67 @@ from pydantic import BaseModel
 from typing import Optional
 import random
 
-
+from .schemas import UserBaseSchema, UserloginSchema, DeleteAccountSchema
 
 router = APIRouter(prefix="/api/v1")
 
 
 
+"""the endpoint will created an account for user and assign tokens"""
+@router.post("/register")
+async def user_register(request: UserBaseSchema,db:Session = Depends(get_db)):
+    if db.query(User).filter_by(email=request.email.lower()).first():
+                raise HTTPException(
+                     status_code=status.HTTP_409_CONFLICT, detail="email already exists")
+    
+    user_obj = User(email=request.email.lower(),
+            full_name=request.full_name,
+            national_id=request.national_id)  
+    user_obj.set_password(request.password)
+    db.add(user_obj)
+    db.commit()
+    db.refresh(user_obj)
+
+    access_token = generate_access_token(user_obj.id)
+    refresh_token = generate_refresh_token(user_obj.id)    
+
+    return JSONResponse(
+            content={
+                "detail": "user registered successfully",
+                "access": access_token,
+                "refresh": refresh_token,
+            },
+            status_code=status.HTTP_201_CREATED,
+    )
 
 
 
+
+
+"""this is an API for login and take an access token"""
 @router.post("/login")
-def login(username: str, password: str, response: Response, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.username == username).first()
-    if not user or not pwd_context.verify(password, user.password_hash):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+async def user_login(request: UserloginSchema, db: Session = Depends(get_db)):
+    try:
+        user_obj = (
+            db.query(User).filter_by(email=request.email.lower()).first()
+        )
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="password is invalid"
+        )
+    if not user_obj or not user_obj.verify_password(request.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password",
+        )
 
-    access_token = generate_access_token(user.id)
-    refresh_token = generate_refresh_token(user.id)
+    access_token = generate_access_token(user_obj.id)
 
-
-    response.set_cookie(
-        key="access_token",
-        value=access_token,
-        httponly=True,
-        secure=True,
-        samesite="lax",
-        max_age=300,  # 5 دقیقه
-        path="/"
-    )
-    response.set_cookie(
-        key="refresh_token",
-        value=refresh_token,
-        httponly=True,
-        secure=True,
-        samesite="lax",
-        max_age=86400,  # 1 روز
-        path="/"
+    return JSONResponse(
+        content={"detail": "logged in successfully", "access_token": access_token},status_code=status.HTTP_202_ACCEPTED
     )
 
-    return {"msg": "Login successful"}
+
 
 
 
@@ -89,8 +111,24 @@ def refresh(request: Request, response: Response, db: Session = Depends(get_db))
 
 
 
-@router.post("/logout")
-def logout(response: Response):
-    response.delete_cookie("access_token", path="/")
-    response.delete_cookie("refresh_token", path="/")
-    return {"msg": "Logged out"}
+
+
+
+@router.delete("/you", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_account(
+    request: DeleteAccountSchema,
+    db: Session = Depends(get_db),
+    user: User = Depends(get_authenticated_user)
+):
+    
+    if not user.verify_password(request.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid password"
+        )
+
+
+    db.delete(user)
+    db.commit()
+
+    return None  

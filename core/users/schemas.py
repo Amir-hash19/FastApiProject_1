76d3aba92bcbb1fe3744464f4fdcce77c0fc50c1
amirhoseinheydari <amirhosein.hydri1381@email.com>
2025-10-14 15:54:12 +0330

    feat: user flake8 and black

--- core/users/schemas.py
+++ core/users/schemas.py
@@ -1,19 +1,14 @@
 from pydantic import BaseModel, Field, field_validator
 
 
-
 class UserBaseSchema(BaseModel):
-    email: str = Field(
-        ...,max_length=170,description="use email for validate user"
-    )
-    full_name: str = Field(
-        ...,max_length=200,description="set fullname for user"
-    )
-    national_id: str = Field(...,max_length=10,min_length=0)
+    email: str = Field(..., max_length=170, description="use email for validate user")
+    full_name: str = Field(..., max_length=200, description="set fullname for user")
+    national_id: str = Field(..., max_length=10, min_length=0)
     password: str = Field(max_length=70, min_length=8)
-    password_confirm: str = Field(max_length=70,min_length=8,
-    description="password confirmation")
-
+    password_confirm: str = Field(
+        max_length=70, min_length=8, description="password confirmation"
+    )
 
     @field_validator("password_confirm")
     def check_passwords_match(cls, password_confirm, validation):
@@ -22,14 +17,9 @@ class UserBaseSchema(BaseModel):
         return password_confirm
 
 
-
-
 class UserloginSchema(BaseModel):
     email: str = Field(..., max_length=128, description="email of the user")
     password: str = Field(..., max_length=70, min_length=8)
-
-
-
 
 
 class DeleteAccountSchema(BaseModel):
